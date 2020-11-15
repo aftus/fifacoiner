69fb5584fa12cecb88c8a84a7579a22f22918452
AFTUS <aftusenginner@gmail.com>
2020-11-15 10:48:38 +0330

    version 12 vibrate on click

--- js/app.js
+++ js/app.js
@@ -1,18 +1,8 @@
-function openMenu(){document.getElementById("menu").style.width="60px"}function closeMenu(){document.getElementById("menu").style.width="0"}function openInfo(){document.getElementById("info").style.display="flex"}function closeInfo(e){e.target==document.getElementById("info")&&(document.getElementById("info").style.display="none")}window.addEventListener("click",closeInfo);
+// for vibrate on click
+function vibrate (ms) {
+  navigator.vibrate(ms);
+}
 
-    // calculator start
-    function dis(val) 
-    { 
-        document.getElementById("calc_result").value+=val 
-    } 
-    function solve() 
-    { 
-        let x = document.getElementById("calc_result").value 
-        let y = eval(x) 
-        document.getElementById("calc_result").value = y 
-    } 
-    function clr() 
-    { 
-        document.getElementById("calc_result").value = "" 
-    } 
-    // calculator end
+function vibratePattern () {
+  navigator.vibrate([300, 100, 300, 100, 300]);
+}
